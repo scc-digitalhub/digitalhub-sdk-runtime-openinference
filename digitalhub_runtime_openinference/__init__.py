6ae91c3a4e15ce24f5835199e31ad8473752ef4d
kazhamiakin <kazhamiakin@gmail.com>
2026-02-27 16:39:13 +0100

    import fix

--- digitalhub_runtime_openinference/__init__.py
+++ digitalhub_runtime_openinference/__init__.py
@@ -3,13 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 from digitalhub_runtime_openinference.entities._commons.enums import EntityKinds
 from digitalhub_runtime_openinference.entities.function.openinference.builder import FunctionOpeninferenceBuilder
-from digitalhub_runtime_openinference.entities.run.serve.builder import RunOpeninferenceServeBuilder
+from digitalhub_runtime_openinference.entities.run.serve.builder import RunOpeninferenceRunServeBuilder
 from digitalhub_runtime_openinference.entities.task.serve.builder import TaskOpeninferenceServeBuilder
 
 entity_builders = (
     (EntityKinds.FUNCTION_OPENINFERENCE.value, FunctionOpeninferenceBuilder),
     (EntityKinds.TASK_OPENINFERENCE_SERVE.value, TaskOpeninferenceServeBuilder),
-    (EntityKinds.RUN_OPENINFERENCE_SERVE.value, RunOpeninferenceServeBuilder),
+    (EntityKinds.RUN_OPENINFERENCE_SERVE.value, RunOpeninferenceRunServeBuilder),
 )
 
 try:
